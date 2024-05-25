438f835bdd2a8a973fbafc7c4ebd2eaba613437b
Ujjwal <sinhaujjwal175@gmail.com>
2024-05-25 23:28:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React, {useState,useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
+
+export default function App() {
+  const [images,setImages]=useState([]);
+  //const [currentPage, setCurrentPage] = useState(1);
+  //const [imagesPerPage] = useState(9);  // Number of images to display per page
+  const [isLoading,setisLoading]=useState(true);
+  const [term,setTerm]=useState('');
+  
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${
+      process.env.REACT_APP_PIXABAY_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res=>res.json())
+      .then(data=>{
+        setImages(data.hits);
+        setisLoading(false);
+      })
+      .catch(err=>console.log(err));
+  },[term]);//whenever term changes this will work again
+
+ /* // Get current images based on pagination
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container mx-auto">
+    <ImageSearch searchText={(text)=>setTerm(text)}/>
+     {!isLoading && images.length ===0 && <h1 className="text-6xl text-container mx-auto mt-32">No Images Found</h1> }
+     {isLoading ? <h1 className="text-6xl text-container mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+         {images.map(image=>(
+          <ImageCard key={image.id} image={image}/>
+         ))}
+     </div>}
+   </div>
   );
 }
-
-export default App;
